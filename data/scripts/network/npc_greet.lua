38b64e837328b6c37e458dd4c4d4e68dc7b57542
fusion32 <marcopuzziello@gmail.com>
2026-01-22 15:21:33 -0300

    npc greet robustness

--- data/scripts/network/npc_greet.lua
+++ data/scripts/network/npc_greet.lua
@@ -1,7 +1,13 @@
 local handler = PacketHandler(0xEE)
 
 function handler.onReceive(player, msg)
-	player:say("hi", TALKTYPE_SAY)
+	-- NOTE(fusion): Send greet message to targeted NPC to avoid conflicts with
+	-- other surrounding NPCs, and to the player for feedback.
+	local npc = Npc(msg:getU32())
+	if npc then
+		player:say("hi", TALKTYPE_SAY, false, npc)
+	end
+	player:say("hi", TALKTYPE_SAY, false, player)
 end
 
 handler:register()
